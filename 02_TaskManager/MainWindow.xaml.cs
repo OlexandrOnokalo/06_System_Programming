ef63a811108986e64528c84a9094e78157fdbe7c
OlexandrOnokalo <Onoka_nd96@student.itstep.org>
2025-09-30 22:52:29 +0300

    Add project files.

---
+++ 02_TaskManager/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _02_TaskManager
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            grid.ItemsSource = Process.GetProcesses();
+            //(grid.SelectedItem as Process)
+            string message = " ";
+            message += "Hellloooooooooo";
+           
+            MessageBox.Show(message, "Process info", MessageBoxButton.OK, MessageBoxImage.Information);
+            MessageBox.Show(nameProc.Text);
+        }
+    }
+}
