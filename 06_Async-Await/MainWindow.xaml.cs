82e9a394cfae31d5cf5c0a9892bc8c3bf5e65292
OlexandrOnokalo <Olexandr.onokalo@gmail.com>
2025-10-08 17:58:12 +0300

    06 LAB

---
+++ 06_Async-Await/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _06_Async_Await
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string sourcePath = from.Text;
+            string destPath = to.Text;
+            File.Copy(sourcePath, destPath, overwrite: true);
+        }
+    }
+}
