caacd9e0ac5aadfc0d0dbde50c778932c5f914b0
OlexandrOnokalo <Onoka_nd96@student.itstep.org>
2025-10-14 19:16:36 +0300

    07 HW

--- 07_CopyFileManager/MainWindow.xaml.cs
+++ 07_CopyFileManager/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Win32;
 using Microsoft.WindowsAPICodePack.Dialogs;
 using PropertyChanged;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,19 +18,19 @@ namespace _07_CopyFileManager
         public MainWindow()
         {
             InitializeComponent();
-            model.Source = sourceTb.Text = @"C:\Users\helen\Downloads\1GB.bin";
-            model.Destination = DestTb.Text = @"C:\Users\helen\Desktop\Test";
-            model.Progress =0;
+            model.Source = sourceTb.Text = @"D:\My Documents\Temp\Temp1.zip";
+            model.Destination = DestTb.Text = @"D:\My Documents\Temp2";
+            model.Progress = 0;
             this.DataContext = model;
-           
+
         }
 
         private void SourceBtn(object sender, RoutedEventArgs e)
         {
             OpenFileDialog fileDialog = new OpenFileDialog();
-            if (fileDialog.ShowDialog() == true) 
+            if (fileDialog.ShowDialog() == true)
             {
-                //MessageBox.Show(fileDialog.FileName);
+               
                 model.Source = sourceTb.Text = fileDialog.FileName;
             }
         }
@@ -37,34 +38,40 @@ namespace _07_CopyFileManager
         private void DestBtn(object sender, RoutedEventArgs e)
         {
             CommonOpenFileDialog dialog = new CommonOpenFileDialog();
-            dialog.IsFolderPicker = true;   
-            //CommonFileDialogResult res =  dialog.ShowDialog();
-            if(dialog.ShowDialog() == CommonFileDialogResult.Ok )
+            dialog.IsFolderPicker = true;
+            
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
             {
                 model.Destination = DestTb.Text = dialog.FileName;
             }
         }
 
-        private async  void CopyBtn(object sender, RoutedEventArgs e)
+        private async void CopyBtn(object sender, RoutedEventArgs e)
         {
-            //C:\Users\helen\Desktop\Testtest.bin
-           // Path.Combine(Source, Destination);
+            
             string filename = Path.GetFileName(model.Source);
             string destFilename = Path.Combine(model.Destination, filename);
-            await CopyFileAsync(model.Source, destFilename);
-            MessageBox.Show("Completed!!!");
             
+            CopyProcessecInfo info = new CopyProcessecInfo()
+            {
+                Filename = filename,
+                Percentage = 0
+            };
+
+            model.AddProcess(info);
+            await CopyFileAsync(model.Source, destFilename, info);
+            MessageBox.Show("Completed!!!");
+
         }
 
-        private Task CopyFileAsync(string src, string dest)
+        private Task CopyFileAsync(string src, string dest, CopyProcessecInfo info)
         {
             return Task.Run(() =>
             {
-                //File.Copy(s, d, true);
-                //2 - FileStream
+                
                 using FileStream srcStream = new FileStream(src, FileMode.Open, FileAccess.Read);
                 using FileStream desStream = new FileStream(dest, FileMode.Create, FileAccess.Write);
-                byte[] buffer = new byte[1024 * 8];//8Kb
+                byte[] buffer = new byte[1024 * 8];
                 int bytes = 0;
                 do
                 {
@@ -72,23 +79,40 @@ namespace _07_CopyFileManager
                     desStream.Write(buffer, 0, bytes);
 
                     float percentage = desStream.Length / (srcStream.Length / 100);
-                    //progress.Value = percentage;    
-                    model.Progress = percentage;    
+                       
+                    model.Progress = percentage;
+                    info.Percentage = percentage;
 
 
                 } while (bytes > 0);
-          
-  
+
+
             });
-            
+
         }
     }
     [AddINotifyPropertyChangedInterface]
     class ViewModel
     {
+        private ObservableCollection<CopyProcessecInfo> processes;
         public string Source { get; set; }
         public string Destination { get; set; }
         public float Progress { get; set; }
         public bool IsWaiting => Progress == 0;
+        public IEnumerable<CopyProcessecInfo> Processes => processes;//get - readonly
+        public ViewModel()
+        {
+            processes = [];
+        }
+        public void AddProcess(CopyProcessecInfo info)
+        {
+            processes.Add(info);
+        }
+    }
+    [AddINotifyPropertyChangedInterface]
+    class CopyProcessecInfo
+    {
+        public string Filename { get; set; }
+        public float Percentage { get; set; }
     }
 }
